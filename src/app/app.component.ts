4fd02a482c67aa6fcea33fb71cf84215404e3480
ramonruizdev <ramonruizrc@gmail.com>
2022-07-17 17:15:15 -0500

    String interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Ramón';
+  age = 20;
+  img = 'https://www.w3schools.com/howto/img_avatar.png'
 }
