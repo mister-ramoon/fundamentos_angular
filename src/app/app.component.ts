909524dd3486ed7620327d707be6f946adfafa32
ramonruizdev <ramonruizrc@gmail.com>
2022-07-18 09:26:52 -0500

    *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,8 @@ export class AppComponent {
     age: 20,
     avatar: 'https://www.w3schools.com/howto/img_avatar.png'
   }
+  names: string[] = ['Juan', 'Ramón', 'Shrek'];
+  newName = '';
 
   toggleButton() {
     this.btnDisabled = !this.btnDisabled;
@@ -32,5 +34,14 @@ export class AppComponent {
   changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1);
   }
 }
